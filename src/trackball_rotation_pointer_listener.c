597d75c1501d71ada4082b0c358bcd955cedd337
mjmjm0101 <mjmjm0101@users.noreply.github.com>
2026-02-13 17:21:06 +0900

    save

--- src/trackball_rotation_pointer_listener.c
+++ src/trackball_rotation_pointer_listener.c
@@ -1,14 +1,14 @@
 #include <zephyr/logging/log.h>
 
 #include <zmk/event_manager.h>
-#include <zmk/events/pointer_moved.h>
+#include <zmk/events/pointer_move.h>
 
 #include <kimiboard/trackball_rotation.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static int trackball_rotation_pointer_listener(const zmk_event_t *eh) {
-    struct zmk_pointer_moved *ev = as_zmk_pointer_moved(eh);
+    struct zmk_pointer_move_event *ev = as_zmk_pointer_move_event(eh);
 
     if (ev == NULL) {
         return ZMK_EV_EVENT_BUBBLE;
@@ -18,14 +18,14 @@ static int trackball_rotation_pointer_listener(const zmk_event_t *eh) {
     int16_t rotated_x = 0;
     int16_t rotated_y = 0;
     
-    trackball_rotate_delta(ev->x, ev->y, &rotated_x, &rotated_y);
+    trackball_rotate_delta(ev->dx, ev->dy, &rotated_x, &rotated_y);
     
     // 補正されたベクトルを適用
-    ev->x = rotated_x;
-    ev->y = rotated_y;
+    ev->dx = rotated_x;
+    ev->dy = rotated_y;
     
     return ZMK_EV_EVENT_BUBBLE;
 }
 
 ZMK_LISTENER(trackball_rotation_pointer, trackball_rotation_pointer_listener);
-ZMK_SUBSCRIPTION(trackball_rotation_pointer, zmk_pointer_moved);
+ZMK_SUBSCRIPTION(trackball_rotation_pointer, zmk_pointer_move_event);
